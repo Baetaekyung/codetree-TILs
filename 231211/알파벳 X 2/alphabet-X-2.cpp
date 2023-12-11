9f4d92de1e33788f2319fe14bc6fadbdcdede17f
Baetaekyung <127454650+Baetaekyung@users.noreply.github.com>
2023-12-11 19:52:12 +0900

    Title: 알파벳 X 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 231211/알파벳 X 2/alphabet-X-2.cpp
+++ 231211/알파벳 X 2/alphabet-X-2.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <list>
 
 using namespace std;
 
 int main() {
-    cout << 0;
+    string str;
+    cin >> str;
+    vector<char>check;
+    int answer = 0;
+    int i, j;
+    for (i = 0; i < str.size(); i++) {
+        for (j = i + 1; j < str.size(); j++) {
+            if (str[i] == str[j]) {
+                str.erase(j, 1);
+                break;
+            }
+            auto itr = find(check.begin(), check.end(), str[j]);
+            if (itr != check.end()) {
+                check.erase(itr);
+            }
+            else {
+                check.push_back(str[j]);
+            }
+        }
+        answer += check.size();
+        check.clear();
+    }
+    cout << answer;
+
     return 0;
 }
